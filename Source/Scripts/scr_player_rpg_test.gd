cb91b5a7bdbc34ae3ec4a1ce8e16191e32068cb2
Kyle Brick <kylebrickgames@gmail.com>
2026-06-01 19:15:58 -0700

    Pushable Blocks
    -Added block rigidbody nodes that the player can push in the cabin scene.

--- Source/Scripts/scr_player_rpg_test.gd
+++ Source/Scripts/scr_player_rpg_test.gd
@@ -4,15 +4,17 @@ class_name Player
 #Vars
 @onready 	var anim_spr: 	AnimatedSprite2D 	= $Sprite;
 @export 	var walk_spd: 	int 				= 50;
-@export		var run_spd: 	int					= walk_spd*2;
-var sprint: bool = false;
+@export		var push_str:	int					= 80;
+var run_spd: 				int					= walk_spd*2;
+var sprint: 				bool 				= false;
 
 #Create
 func _ready() -> void:
 	position = Director.pl_spawn_pos;
+	#Engine.max_fps = 15;
 
-#Step - Phy
-func _process(delta: float) -> void:
+#Step
+func _physics_process(delta: float) -> void:
 	
 	#Walk / Run
 	var dir := Input.get_vector("move_left","move_right","move_up","move_down");
@@ -28,5 +30,14 @@ func _process(delta: float) -> void:
 	elif(velocity.y < 0): anim_spr.play("walk_up");
 	elif(velocity == Vector2.ZERO):
 		anim_spr.stop(); anim_spr.frame = 0;
+	
+	#Check last collision
+	#If it's the block... push it.
+	var col: KinematicCollision2D = get_last_slide_collision();
+	if(col != null):
+		var col_node = col.get_collider(); #Get block node ID
+		if(col_node.is_in_group("Blocks")):
+			var col_norm: Vector2 = col.get_normal();
+			col_node.apply_central_force(-col_norm*push_str);
 	
 	move_and_slide();
